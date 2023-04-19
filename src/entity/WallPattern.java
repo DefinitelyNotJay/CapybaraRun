de01dc33087d93295c3a355b73fdefc3d797a17a
siroratN <siroratnbs@gmail.com>
2023-04-19 22:35:47 +0700

    .

--- src/entity/WallPattern.java
+++ src/entity/WallPattern.java
@@ -5,13 +5,16 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 import main.GamePanel;
 
 public class WallPattern {
     private GamePanel gp;
-    private WallGround w1, w2, w3, w4;
+    private WallGround w1, w2, w3;
 
-    private WallHanging wg1, wg2;
+    private WallHanging wg1;
     private ArrayList<Wall> wallPattern;
     
     public WallPattern(GamePanel gp){
@@ -21,13 +24,19 @@ public class WallPattern {
     }
     
     public void loadWalls(){
+        Set<Object> WallSet = new HashSet<Object>();
+        Set<Object> collect = new HashSet<Object>();
         w1 = new WallGround(gp,330, 250, gp.tileSize, gp.tileSize);
         w2 = new WallGround(gp,550, 250, gp.tileSize, gp.tileSize);
         w3 = new WallGround(gp,820, 250, gp.tileSize, gp.tileSize);
-        // wg1 = new WallHanging(gp,1120, 90, gp.tileSize*2, gp.tileSize*2+60);
-        wg2 = new WallHanging(gp,1120, 90, gp.tileSize*8, gp.tileSize*2+60);
-        
-        Collections.addAll(wallPattern, w1, w2, w3, wg2);
+        wg1 = new WallHanging(gp,1120, 180, gp.tileSize, gp.tileSize+20);
+        collect.add(w1);
+        collect.add(w2);
+        collect.add(w3);
+        collect.add(wg1);
+        WallSet.addAll(collect);//kuysus
+
+        Collections.addAll(wallPattern);
     }
     
 
