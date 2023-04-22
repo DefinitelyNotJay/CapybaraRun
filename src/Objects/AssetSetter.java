7a4f3ce2727504481e98cd5cb0eb1407ee1d8623
Tendou26 <jjbrathers3@gmail.com>
2023-04-22 17:12:02 +0700

    creating object constructor

--- src/Objects/AssetSetter.java
+++ src/Objects/AssetSetter.java
@@ -8,8 +8,8 @@ public class AssetSetter{
         this.gp = gp;
     }
     public void serObject(){
-        gp.obj[0] = new Obj_big();
-        gp.obj[0].mapX = 23*gp.tileSize;
-        gp.obj[0].mapY = 7*gp.tileSize;
+        gp.obj[0] = new Obj_big(gp, 23*gp.tileSize, 7*gp.tileSize, gp.tileSize, gp.tileSize);
+        // gp.obj[0].mapX = 23*gp.tileSize;
+        // gp.obj[0].mapY = 7*gp.tileSize;
     }
 }
--- src/Objects/Obj_big.java
+++ src/Objects/Obj_big.java
@@ -2,8 +2,11 @@ package Objects;
 
 import java.io.IOException;
 
+import main.GamePanel;
+
 public class Obj_big extends SuperObjects {
-    public Obj_big(){
+    public Obj_big(GamePanel gp,double x, double y,int sizeX, int sizeY){
+        super(gp, x, y, sizeX, sizeY);
          setName("Big");
     }
 }
