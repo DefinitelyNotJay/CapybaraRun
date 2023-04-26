ae8e534e5b12ad11ebedcbc1f45e41c55a5627a7
PIRAPORN PRASERT <sasayaza@gmail.com>
2023-04-26 21:06:30 +0700

    Create Sound.java

---
+++ src/main/Sound.java
@@ -0,0 +1,36 @@
+package main;
+import java.net.URL;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+public class Sound {
+    private Clip clip;
+    private URL soundURL[] = new URL[30];
+    
+    public Sound(){
+        soundURL[0] = getClass().getResource("/sound/sound/capybara_main.wav");
+    }
+
+    public void setFiles(int i){
+        try {
+            AudioInputStream ais = AudioSystem.getAudioInputStream(soundURL[i]);
+            clip = AudioSystem.getClip();
+            clip.open(ais);
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void playsound(){
+        clip.start();
+    }
+
+    public void loopsound(){
+        clip.loop(Clip.LOOP_CONTINUOUSLY);
+    }
+
+    public void stopsound(){
+        clip.stop();
+    }
+}
