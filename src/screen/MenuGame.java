ea0116744b1fab69f7abed4d7ca312d611086e06
Tendou26 <jjbrathers3@gmail.com>
2023-05-03 20:36:24 +0700

    Menu screen changed and result screen add

--- src/screen/MenuGame.java
+++ src/screen/MenuGame.java
@@ -2,12 +2,17 @@ package screen;
 
 import java.awt.*;
 import javax.swing.*;
+
+import methods.Animations;
+import methods.ScreenTools;
 import methods.Utilz;
 import java.awt.image.BufferedImage;
 
-public class MenuGame extends JPanel {
+public class MenuGame extends JPanel implements Animations, ScreenTools {
 
     public Button[] button;
+    private int aniTick, aniIndex, aniSpeed = 7;
+    private BufferedImage[] runningCapy;
     public final int CREDITS = 0, PLAY = 1, QUIT = 2;
     private BufferedImage bgImg;
 
@@ -17,14 +22,34 @@ public class MenuGame extends JPanel {
 
     }
 
-    public void buttonCreate() {
-        button = new Button[3];
-        button[CREDITS] = new Button(444, 130, 125, 54);
-        button[PLAY] = new Button(418, 70, 125, 54);
-        button[QUIT] = new Button(410, 190, 125, 54);
+    public void update() {
+        updateAnimations();
     }
 
-    private void loadImages() {
+    public void buttonCreate() {
+        button = new Button[3];
+        button[CREDITS] = new Button(570, 230, 125, 54);
+        button[PLAY] = new Button(570, 170, 125, 54);
+        button[QUIT] = new Button(570, 290, 125, 54);
+    }
+
+    @Override
+    public void updateAnimations() {
+        aniTick++;
+        if (aniTick >= aniSpeed) {
+            aniTick = 0;
+            aniIndex++;
+        }
+
+        if (aniIndex >= 7) {
+            aniIndex = 0;
+        }
+    }
+
+    @Override
+    public void loadImages() {
+        runningCapy = Utilz.getRunningImg("/res/player/capybara/capyrun2.png");
+
         bgImg = Utilz.GetImage("/res/screen/menu/menu bg.png");
 
         button[PLAY].setImages("/res/screen/menu/start");
@@ -42,5 +67,7 @@ public class MenuGame extends JPanel {
         for (int i = 0; i < button.length; i++) {
             button[i].draw(g2);
         }
+        g2.drawImage(runningCapy[aniIndex], 590, 380, (int) (32 * 2.5), (int) (34 * 2.5), null);
     }
+
 }
